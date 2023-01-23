cf8d16bf6e97a0b5d2f4b4a100b23463362c0dce
Max Scholz <max@staffbase.com>
2023-01-23 03:24:51 +0100

    optimizations

--- src/App.js
+++ src/App.js
@@ -289,7 +289,6 @@ function App(props) {
 											clearOnEscape
 											options={allRecipes}
 											filterOptions={filterOptions}
-											noOptionsText='Keine Rezepte gefunden :('
 											groupBy={(option) => option.first_letter}
 											getOptionLabel={(option) => option.recipe_title}
 											isOptionEqualToValue={(option, value) =>
